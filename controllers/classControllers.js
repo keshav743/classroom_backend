f7c3b37b04ebc88819cc5cb8310f03c0d09dbc79
keshav743 <keshava0302@gmail.com>
2021-12-28 19:40:27 +0530

    redeploy x10

--- controllers/classControllers.js
+++ controllers/classControllers.js
@@ -282,9 +282,7 @@ module.exports.getAssignmentFileController = async (req, res, next) => {
     });
     console.log(path.join(__dirname, "../", fetchedAssignment.path));
     var file = await fs.createReadStream(
-      path
-        .join(__dirname, "../", path.normalize(fetchedAssignment.path))
-        .replace("/", "\\")
+      path.join(__dirname, "../", fetchedAssignment.path)
     );
     file.pipe(res);
   } catch (err) {
